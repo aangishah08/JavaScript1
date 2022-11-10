2d2f6246fb1eed52f4aa9efab10b7a6fd026159b
aangishah08 <117366652+aangishah08@users.noreply.github.com>
2022-11-10 19:01:39 +0530

    Add files via upload

---
+++ Es6/modulesexport.js
@@ -0,0 +1,9 @@
+
+// const message = () => {
+//     const name = "aangi";
+//     const age = 22;
+//     return name + ' is ' + age + 'years old.';
+//     };
+    const name="Aangi";
+    const age=22;
+    export {name,age};
